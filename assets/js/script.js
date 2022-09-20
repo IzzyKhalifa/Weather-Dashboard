8c27343806d515c1273feff52452905cb19db7da
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-09-20 15:12:48 +1000

    Displays todays weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
-// var temp = document.getElementById('temp');
-// var wind = document.getElementById('wind');
-// var humidity = document.getElementById('humidity');
+const searchButton = document.getElementById("submit");
 var currentDay = moment().format("MM/D/Y");
 
-function getWeather() {
-  var weekWeather =
-    "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=8aff7017aa429b9bc201a6c2e43c57c4&units=metric";
+function getLatLon(cityName) {
+  var latLon =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityName +
+    "&limit=5&cnt=24&appid=8aff7017aa429b9bc201a6c2e43c57c4";
+
+  fetch(latLon)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.length == 0) {
+        console.log(" not available");
+        return;
+      }
+      console.log(data);
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      getWeather(lat, lon);
+      fiveDays(lat,lon);
+    });
+}
+
+function getWeather(lat, lon) {
+  var weekWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&cnt=24&appid=8aff7017aa429b9bc201a6c2e43c57c4&units=metric`;
 
   fetch(weekWeather)
     .then(function (response) {
@@ -13,27 +33,55 @@ function getWeather() {
     })
     .then(function (data) {
       console.log(data);
-      todayWeather(data);
+      todayWeather(data,buildImageSrcFromId(data));
     });
 }
 
-function todayWeather(data) {
+function buildImageSrcFromId(data){
+    return `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+}
+
+function todayWeather(data,imageSrc) {
+  today.innerHTML = "";
   var city = document.createElement("h3");
   var temp = document.createElement("p");
   var wind = document.createElement("p");
   var humidity = document.createElement("p");
-  city.textContent = data.name+' ('+currentDay+')';
-  temp.textContent ='Temp: '+ data.main.temp+ ' °C';
-  wind.textContent ='Wind: '+ data.wind.speed*10 + ' km/h';
-  humidity.textContent ='Humidity: '+ data.main.humidity+ '%';
-  console.log(data.main.temp);
+  var weatherImage = document.createElement('img');
+  weatherImage.src=imageSrc;
+  city.textContent = data.name + " (" + currentDay + ")";
+  temp.textContent = "Temp: " + data.main.temp + " °C";
+  wind.textContent = "Wind: " + data.wind.speed * 10 + " km/h";
+  humidity.textContent = "Humidity: " + data.main.humidity + "%";
   today.append(city);
+  today.append(weatherImage);
   today.append(temp);
   today.append(wind);
   today.append(humidity);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+function fiveDays(lat ,lon){
+                      
+    var weekWeather = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=40&appid=8aff7017aa429b9bc201a6c2e43c57c4&units=metric`;
+
+    fetch(weekWeather)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+}
+
+function fiveDaysForcast(){
+
+}
+
+function execute() {
   var today = document.getElementById("today");
-  getWeather();
-});
+  var name = document.getElementById("Name").value;
+  var cityName = name;
+  getLatLon(cityName);
+  
+}
+searchButton.addEventListener("click", execute);
